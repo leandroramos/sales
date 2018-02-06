4f0511026c7cfdd9a380fbc45f0b1adec7f271c9
Leandro Ramos <leandroramos@usp.br>
2018-02-05 23:50:44 -0200

    Models and migrations working fine

---
+++ database/migrations/2018_02_06_014318_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('price');
+            $table->integer('seller_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('sellers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
